278e78a17ba95951a9298b542f880cdbf0b7a9dd
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-06-18 17:36:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionDemo
+{
+    internal class Program
+    {
+        static void Numbers(int n)
+        {
+            if (n > 0)
+            {
+                Numbers(n - 1);
+                Console.Write(n + " ");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Numbers(n);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
